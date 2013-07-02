4533176f9db13321670670a54588706c8dc1ba70
Nandini Goel <nandinigoel.india@gmail.com>
2013-07-02 21:08:38 +0530

    Created a rails application and generated a scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Consortium::Application.config.secret_token = 'a77bdf38e40f9a49579a0e46760c0c0e38110cc7ca752061d070ccae6646d7643a2ebae52734f1cc0b731e78fc40ccb31a0e372e404918e610828c0068cbf0a7'
